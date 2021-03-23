0bcc94206bbb8030d34c75ebe356860e009cb511
Brian Lopez <brianlopezmedina12@gmail.com>
2021-03-22 20:39:15 -0600

    subida a repo

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIFje5tUjgQJLmywaSAuPoUKmb9cD8PBs",
+    authDomain: "foodsmil.firebaseapp.com",
+    projectId: "foodsmil",
+    storageBucket: "foodsmil.appspot.com",
+    messagingSenderId: "621939755656",
+    appId: "1:621939755656:web:14bf62c3054424b655ad3d",
+    measurementId: "G-JV1V68NML7"
+  };
+
+
+export default firebaseConfig;
